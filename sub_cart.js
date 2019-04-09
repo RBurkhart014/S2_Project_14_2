cc87f8cb3384bb3065e18aa4764ca8ba1b42ff02
Ryan Burkhart <RBurkhart014@west-mec.org>
2019-04-08 17:38:17 -0700

    Final Commit
    Added final for loop & if statement & added comments

--- sub_cart.js
+++ sub_cart.js
@@ -22,12 +22,38 @@
 
 */
 
+//setupcart will be the first thing the page loads
 window.onload = setupCart;
 
 function setupCart() {
       var addButtons = document.getElementsByClassName("addButton");
 
       for (var i = 0; i < addButtons.length; i++) {
-            
+            document.addEventListener("onclick", addItem);
       }
 }
+
+//the loop & function goes through all the functions waiting for you to click and once you click the next function & loop & if's process it
+
+function addItem(e) {
+      var foodItem = e.nextElementSibling.value;
+      var foodID = foodItem.getAttribute("id");
+      var foodDescription = foodItem.cloneNode(true);
+      var cartBox = document.getElementById("cart");
+      var duplicateOrder = false;
+
+      for (var i = 0; i < cartBox.length; i++) {
+            if (cartBox.childNodes == foodID) {
+                  cartBox.firstChild += 1;
+            }
+      }
+
+      if (duplicateOrder !== false) {
+            var orderCount = document.createElement("span");
+            orderCount = "1";
+            foodDescription += orderCount;
+            foodDescription.appendChild(cartBox);
+      }
+}
+
+//all of this basically just processes what you click & adds it into your shopping cart & allows you to order
